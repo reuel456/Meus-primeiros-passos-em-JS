1e484487efe02cbe24aa34122e3d54cff5fcec2b
reuel456 <reuel.eugenio@edu.senai.br>
2026-05-06 14:39:58 -0300

    Exercício 42: Quadrados na Faixa de Valores

--- repeticao/ex41.js
+++
@@ -1,6 +0,0 @@
-const prompt = require('prompt-sync')();
-let raio = Number(prompt("Digite o valor do raio: "));
-let altura = Number(prompt("Digite o valor da altura: "));
-const PI = 3.14159;
-let volume = PI * (raio ** 2) * altura;
-console.log(`O volume calculado é: ${volume.toFixed(2)}`);
